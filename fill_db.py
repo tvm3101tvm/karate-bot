3d0605c58f9cca5d354a77b74b2e0ad3ad916bab
Кукум Мерси <tvm3101@gmail.com>
2026-03-20 18:38:29 +0300

    Update media file_ids

--- fill_db.py
+++ fill_db.py
@@ -69,7 +69,7 @@ def main():
             category='block',
             description='Блок снаружи внутрь (ладонью вниз).',
             video_path='file_id_видео_сотоукэ',
-            gif_path='CgACAgIAAxkBAAIHRGm9W1J111jYKagPiH-J1-BfJOVNAAIIkwACYE3oSRCfIG9lh6sGOgQ',
+            gif_path='CgACAgIAAxkBAAIHImm8mSXUIZ-RpWdUwOBSnCAPgrptAAJCoQACYE3gSbm5_5qcGNWyOgQ',
             audio_path='CQACAgIAAxkBAAIEx2mwLz7gS5zFuwTk_OkqAphJjaA_AAJfnQAC_T14ST8IT1qOwdRHOgQ'
         ),
         Technique(
